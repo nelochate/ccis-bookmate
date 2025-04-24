34478df8d2db3d418508f5431557dd8bc810b65d
Nel Ochate <danmats@gmail.com>
2025-04-24 14:31:22 +0800

    password visible or !

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,7 +1,22 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const visible = ref(false)
+</script>
+
 <template>
   <v-form fast-fail @submit.prevent>
-    <v-text-field label="Email" variant="outlined"></v-text-field>
-    <v-text-field label="Password" type="password" variant="outlined"></v-text-field>
+    <v-text-field density="compact"
+        placeholder="Email address"
+        prepend-inner-icon="mdi-email-outline"
+        variant="outlined"></v-text-field>
+    <v-text-field :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+        :type="visible ? 'text' : 'password'"
+        density="compact"
+        placeholder="Enter your password"
+        prepend-inner-icon="mdi-lock-outline"
+        variant="outlined"
+        @click:append-inner="visible = !visible"></v-text-field>
 
     <v-btn class="mt-2" type="submit" block color="blue-grey-darken-3" prepend-icon="mdi-login">Login</v-btn>
   </v-form>
